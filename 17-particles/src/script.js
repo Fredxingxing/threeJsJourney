783ff4a5d63b322a7d835ad7e1a2375893a55423
xingyu.zhou <33107804+Fredxingxing@users.noreply.github.com>
2022-01-21 17:53:07 +0800

    add particles

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -1,8 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
-import ky from "kyouka";
 
 /**
  * Base
@@ -20,70 +19,71 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
-/**
- * Test cube
- */
-// const cube = new THREE.Mesh(
-//     new THREE.BoxBufferGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
-// scene.add(cube)
-// const particleGeometry = new THREE.SphereBufferGeometry(1, 32, 32);
-
-const particleTexture = textureLoader.load("/textures/particles/2.png");
-
-const particleGeometry = new THREE.BufferGeometry();
-const dimension = 3;
+/*
+* Particles
+* */
+// geometry
+const particlesGeometry = new THREE.BufferGeometry()
 const count = 20000;
-const total = dimension * count;
-const positions = new Float32Array(total);
-const colors = new Float32Array(total);
-for (let i = 0; i < total; i++) {
-  positions[i] = ky.randomNumberInRange(-0.5, 0.5) * 10;
-  colors[i] = ky.randomNumberInRange(0, 1);
-}
-particleGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, dimension)
-);
-particleGeometry.setAttribute(
-  "color",
-  new THREE.BufferAttribute(colors, dimension)
-);
 
-const particleMaterial = new THREE.PointsMaterial({
-  size: 0.1,
-  // color: new THREE.Color("#ff88cc"),
-  alphaMap: particleTexture,
-  transparent: true,
-  depthWrite: false,
-  blending: THREE.AdditiveBlending,
-  vertexColors: true,
-});
-const particles = new THREE.Points(particleGeometry, particleMaterial);
-scene.add(particles);
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
+// particles
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    // color: '#ff88cc',
+    transparent: true,
+    alphaMap: particleTexture,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+    // depthTest: false
+    // alphaTest: 0.001
+})
+
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
@@ -91,10 +91,10 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
 );
 camera.position.z = 3;
 scene.add(camera);
@@ -107,7 +107,7 @@ controls.enableDamping = true;
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -118,29 +118,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = clock.getElapsedTime();
 
-  // particles.rotation.y = elapsedTime * 0.2;
+    // update particles
+    particles.rotation.y = - elapsedTime * 0.25
 
-  for (let i = 0; i < count; i++) {
-    const group = i * dimension;
-    const yAxis = group + 1;
-    const xValue = particleGeometry.attributes.position.array[group];
-    particleGeometry.attributes.position.array[yAxis] = Math.sin(
-      elapsedTime + xValue
-    );
-  }
+    // Update controls
+    controls.update();
 
-  particleGeometry.attributes.position.needsUpdate = true;
+    // Render
+    renderer.render(scene, camera);
 
-  // Update controls
-  controls.update();
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
 };
 
 tick();
